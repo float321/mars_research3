15e2be63a91ac1f55b06769398b31ba62891481b
float321 <187214682+float321@users.noreply.github.com>
2025-05-14 20:45:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def choose_planet(planet_name):
     return render_template('choice.html', planet=planet_name)
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return render_template('results.html', nickname=nickname, level=level, rating=rating)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
